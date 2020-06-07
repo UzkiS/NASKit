264d7cf031c42b32c63d4a84cbb0894957313141
leaplu <leaplu@163.com>
2020-06-07 15:46:56 +0800

    fix problem

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Apache gets grumpy about PID files pre-existing
-rm -f /usr/local/apache2/logs/httpd.pid
-
-exec httpd -DFOREGROUND "$@"  
-#exec smbd --foreground --no-process-group $SMBDOPTIONS
-
